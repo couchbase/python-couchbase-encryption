f3cc7a80414a3052d2232c0356edc77cf5f956ea
Mike Goldsmith <goldsmith.mike@gmail.com>
2018-05-14 17:02:56 +0100

    add license information

--- couchbase/encryption/tests/TestAES256CryptoProvider.py
+++ couchbase/encryption/tests/TestAES256CryptoProvider.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2017 Couchbase, Inc.
+#
+# Use of this software is subject to the Couchbase Inc. Enterprise Subscription License Agreement
+# which may be found at https://www.couchbase.com/ESLA-11132015.
+
 from couchbase.crypto import InMemoryKeyStore
 from couchbase.tests.base import ConnectionTestCase
 from ..AES256CryptoProvider import AES256CryptoProvider
--- couchbase/encryption/tests/TestJavaKeyStore.py
+++ couchbase/encryption/tests/TestJavaKeyStore.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2017 Couchbase, Inc.
+#
+# Use of this software is subject to the Couchbase Inc. Enterprise Subscription License Agreement
+# which may be found at https://www.couchbase.com/ESLA-11132015.
+
 import unittest, os, base64
 from ..JavaKeyStore import JavaKeyStore
 
